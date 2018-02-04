423d7b30429a231aa8457c656fbd7f14eb26b5b1
Dejan Ognjenović <dognjen@yandex.com>
2018-02-04 23:29:30 +0100

    Added an CryptoKitties interface.

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.4.19;
 
 import "./ZombieFactory.sol";
+import "./integrations/CryptoKitties.sol";
 
 contract ZombieFeeding is ZombieFactory {
+
+    address cryptoKittiesAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(cryptoKittiesAddress);
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
@@ -10,7 +14,7 @@ contract ZombieFeeding is ZombieFactory {
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
-        
+
         _createZombie("NoName", newDna);
     }
 
