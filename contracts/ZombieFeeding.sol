b3677c776dcd9ba28d4320d33c3f3a620f7c3a5d
Dejan Ognjenović <dognjen@yandex.com>
2018-02-10 17:02:50 +0100

    Added new method and usage of kripto kitties

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -8,14 +8,26 @@ contract ZombieFeeding is ZombieFactory {
     address cryptoKittiesAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(cryptoKittiesAddress);
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
 
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
 
+        // change the last two digits to 99 (cat zombie)
+        if (keccak256(_species) == keccak256("kitty")) {
+            newDna = newDna - newDna % 100 + 99;
+        }
+
         _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 
 }
